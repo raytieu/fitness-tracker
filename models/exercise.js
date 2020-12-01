064be7c0f0813c2bcd5b59559745a3421f5d9bd4
Ray Tieu <ray.tieu@gmail.com>
2020-11-30 17:57:11 -0800

    added models

---
+++ models/exercise.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String
+        },
+        name: {
+          type: String
+        },
+        duration: {
+          type: Number
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  }
+);
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
