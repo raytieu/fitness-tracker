2a6bd5548a2210fc472f7adb0d4c50c0148fd55f
Ray Tieu <ray.tieu@gmail.com>
2020-11-30 17:42:08 -0800

    added server file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
